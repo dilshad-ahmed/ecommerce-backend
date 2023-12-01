29dacf1d4b2543a419d79f271c9c98963f90b0a4
dilshad-ahmed <dilshad7427@gmail.com>
2023-12-02 00:29:55 +0530

    commented frontend path

--- backend/app.js
+++ backend/app.js
@@ -48,13 +48,13 @@ app.use("/api/v1", Payment);
 
 // **********  Deployment  **********
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  console.log("dirname===>", __dirname);
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-    // res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-  });
+if (process.env.NODE_ENV !== "production") {
+  // app.use(express.static(path.join(__dirname, "../frontend/build")));
+  // console.log("dirname===>", __dirname);
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  //   // res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  // });
   app.get("*", (req, res) => {
     res.send("api is working");
   });
